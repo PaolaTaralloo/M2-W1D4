1b5fda57d389dc9b032723afe3946fd93538d812
PaolaTarallo <paola.trll@gmail.com>
2024-11-19 18:37:00 +0100

    commit2

--- s1p2-scaricabile.js
+++ s1p2-scaricabile.js
@@ -78,7 +78,9 @@ console.log(differenza)
 let name1 = "john"
 let name2 = "John"
 let disuguaglianza = name1 !== name2
+console.log(disuguaglianza)
 
 let uguaglianza = name1.toLowerCase() === name2.toLowerCase()
+console.log(uguaglianza)
 
 */
